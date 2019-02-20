edf6369871033eb557db9a29db3f89a174e11ba8
zhangshuo <zhangshuo@zhangshuodeMacBook-Air.local>
2019-02-20 08:49:44 +0800

    2.20作业

---
+++ src/require.js
@@ -0,0 +1,13 @@
+
+function require(arr, ck) {
+    arr.forEach(function (item) {
+        var src = document.createElement("script")
+        src.src = item
+    })
+    
+}
+
+
+function define(arr,ck) {
+    
+}
